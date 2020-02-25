7bdd5e850231a4ba69b0e510a74245c17abaece0
hagare <helbishlawi@gmail.com>
2020-02-25 15:32:37 -0500

    upd node cons compile contract dwnld votingblkgit

---
+++ Day1of100/compile.sh
@@ -0,0 +1,10 @@
+## Compile the Contract
+## compile contract with nodejs console
+## open nodejs console
+#node 
+#> Web3 = require('web3')
+#> web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+
+## Confirm that the web3 object is initialized and can communicate with the blockchain
+## using the 'accounts' method on the object
+#> web3.eth.accounts
