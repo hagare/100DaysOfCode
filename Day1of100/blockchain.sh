bd544441d45f7e3ae969565d56c02d248382b2bc
hagare <helbishlawi@gmail.com>
2020-02-25 15:07:53 -0500

    create a bash script

---
+++ Day1of100/blockchain.sh
@@ -0,0 +1,19 @@
+# Check NPM and NODE versions
+#echo "npm -v"
+#npm -v
+#echo "node -v"
+#node -v
+
+# Install Ganache CLI
+# personal blockchain for Ethereum development
+# uses ethereumjs to simulate full client behavior 
+#npm install -g ganache-cli
+
+# use Ganache CLI by https://github.com/trufflesuite/ganache-cli
+# ganache-cli <options>
+
+# Install web3
+npm install web3
+
+# Install Solidity code compiler: using solc
+npm install solc
