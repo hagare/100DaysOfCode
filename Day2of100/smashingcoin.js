4ccfa42ba2dc128914d2783cacd0a8b933b568b9
hagare <helbishlawi@gmail.com>
2020-02-27 09:02:28 -0500

    test the blockchain by adding new instances

--- Day2of100/smashingcoin.js
+++ Day2of100/smashingcoin.js
@@ -29,3 +29,9 @@ class CryptoBlockchain{
     this.blockchain.push(newBlock);
   }
 }
+
+let smashingCoin = new CryptoBlockchain();
+smashingCoin.addNewBlock(new CryptoBlock(1, "01/06/2020", {sender: "Iris Ljesnjanin", recipient: "Cosima Mielke", quantity: 50}));
+smashingCoin.addNewBlock(new CryptoBlock(2, "01/07/2020", {sender: "Vitaly Friedman", recipient: "Ricardo Gimenes", quantity: 100}) );
+console.log(JSON.stringify(smashingCoin, null, 4));
+
